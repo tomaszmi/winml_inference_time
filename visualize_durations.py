b699328d83f7f29d4b4802f4050f4935c3c46d3f
tomaszmi <4892955+tomaszmi@users.noreply.github.com>
2021-04-27 13:37:44 +0200

    visualizing multiple result files at once

--- visualize_durations.py
+++ visualize_durations.py
@@ -1,21 +1,35 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
-inference_times  = []
+def get_files():
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--file', action='append', help='Path to file with durations')
+	args = parser.parse_args()
+	if not args.file:
+		args.file = ['winml_durations.txt']
+	return args.file
 
-with open('winml_durations.txt') as f:
-	for line in f.readlines():
-		if len(line) != 0:
-			(nap_time, duration) = line.split()
-			inference_times.append((int(nap_time), int(duration)))
+def get_inference_times_from_file(file_path):
+	inference_times  = []
+	with open(file_path) as f:
+		for line in f.readlines():
+			if len(line) != 0:
+				(nap_time, duration) = line.split()
+				inference_times.append((int(nap_time), int(duration)))
+	return inference_times
 
-df = pd.DataFrame(inference_times, columns=['nap_time', 'inference_dur'])
-groupped = df.groupby('nap_time').agg({'inference_dur': ['min', 'max', 'mean', 'std']})
-print(groupped.to_string())
-p = df.boxplot(column=['inference_dur'], by='nap_time')
-p.set_xlabel('nap_time [ms]')
-p.set_ylabel('duration [ms]')
-plt.title('winml inference durations') 
-plt.suptitle('') 
+for file_path in get_files():
+	inference_times = get_inference_times_from_file(file_path)
+	df = pd.DataFrame(inference_times, columns=['nap_time', 'inference_dur'])
+	groupped = df.groupby('nap_time').agg({'inference_dur': ['min', 'max', 'mean', 'std']})
+	print(groupped.to_string())
+	p = df.boxplot(column=['inference_dur'], by='nap_time')
+	p.set_xlabel('nap_time [ms]')
+	p.set_ylabel('duration [ms]')
+
+	plt.title(file_path) 
+	plt.suptitle('') 
+
 plt.show()
